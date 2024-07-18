4cbb65d5ed3d0178b28306f46b4055b6077c2ca8
Mitra HSH <mitrahooshmand4824@gmail.com>
2024-07-18 23:09:48 +0330

    practice short circuiting

--- index.js
+++ index.js
@@ -41,9 +41,20 @@ const game = {
   },
 };
 
-const players1 = [...game.players[0]];
-const players2 = [...game.players[1]];
+// const players1 = [...game.players[0]];
+// const players2 = [...game.players[1]];
+const [players1, players2] = game.players;
 const [gk1, ...fieldplayers1] = players1;
 const [gk2, ...fieldplayers2] = players2;
+const allPlayers = [...players1, ...players2];
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+// console.log((team1 ||= "winner"));
+team1 < team2 && console.log("team1 is winwer");
+team1 > team2 && console.log("team2 is winer");
+const printGoals = function (...params) {
+  console.log(`${params.length} is scored`);
+};
 
-console.log(gk2, fieldplayers2);
+printGoals("sara", "dara");
