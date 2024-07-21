cd716c0a0c390495aeaac2c95d0c75b5b0511fee
Mitra HSH <mitrahooshmand4824@gmail.com>
2024-07-21 11:49:22 +0330

    start learning about SASS and also practice MAP in js as well

--- index.js
+++ index.js
@@ -71,3 +71,17 @@ for (let [team, score] of Object.entries(game.odds)) {
   const teamName = team === "x" ? "draw" : `victory ${game[team]}`;
   console.log(`Odd of ${teamName} is: ${score} `);
 }
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
