a2934e504643d1fdf72ba2d6ace8cebb190c75d5
Mitra HSH <mitrahooshmand4824@gmail.com>
2024-07-29 21:01:21 +0330

    a method added to object

--- index.js
+++ index.js
@@ -232,7 +232,17 @@ const poll = {
   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
   // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
- 
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n ${this.options.join("\n")} \n (write the numbers✍)`
+      )
+    );
+    typeof answer === "number" &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    console.log(this.answers);
+  },
 };
 
 poll.registerNewAnswer();
